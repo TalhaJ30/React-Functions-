d51a741a3fc9b289e0039651124008f8299612a2
Talha Javed <talhaj1515@gmail.com>
2025-07-05 11:07:52 +0500

    added change

--- src/Key.jsx
+++ src/Key.jsx
@@ -11,9 +11,9 @@ const Key = ({onClickButton}) => {
                         <button
                             className={`w-14 h-14 font-normal rounded-xl shadow-md transition-all duration-300 transform hover:scale-105 focus:outline-none focus:ring-2 text-2xl flex-wrap ${
                                 item === 'AC' 
-                                    ? 'border-2 border-red-400 text-red-400 bg-red-50 hover:bg-gradient-to-r hover:from-red-400 hover:to-red-500 hover:text-white focus:ring-red-400 pl-1.5 pr-1.5 h-10 w-20 hover:shadow-lg hover:shadow-red-300' 
+                                    ? ' hover:scale-105 hover:rotate-3 border-2 border-red-400 text-red-400 bg-red-50 hover:bg-gradient-to-r hover:from-red-400 hover:to-red-500 hover:text-white focus:ring-red-400 pl-1.5 pr-1.5 h-10 w-20 hover:shadow-lg hover:shadow-red-300' 
                                     : item === '=' 
-                                        ? 'border-2 border-green-400 text-green-400 bg-green-50 hover:bg-gradient-to-r hover:from-green-400 hover:to-green-500 hover:text-white focus:ring-green-400 hover:shadow-lg hover:shadow-green-300' 
+                                        ? ' hover:scale-105 hover:rotate-3 border-2 border-green-400 text-green-400 bg-green-50 hover:bg-gradient-to-r hover:from-green-400 hover:to-green-500 hover:text-white focus:ring-green-400 hover:shadow-lg hover:shadow-green-300' 
                                         : item === '+' || item === '-' || item === '*' || item === '/' 
                                             ? 'border-2 border-cyan-400 text-cyan-600 bg-cyan-50 hover:bg-gradient-to-r hover:from-cyan-400 hover:to-cyan-500 hover:text-white focus:ring-cyan-400 hover:scale-105 hover:rotate-3 hover:shadow-lg hover:shadow-cyan-300' 
                                             : 'border-2 border-blue-400 text-blue-700 bg-blue-100 hover:bg-gradient-to-r hover:from-blue-400 hover:to-blue-500 hover:text-white focus:ring-blue-400 hover:shadow-lg hover:shadow-blue-300'
